7b3c88bde84654a46fbf4486b667760a373f760c
OjoSirisak <57130500078@st.sit.kmutt.ac.th>
2017-09-12 01:05:31 +0700

    add search

---
+++ routes/search.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const Boom = require('boom');
+const uuid = require('node-uuid');
+const Joi = require('joi');
+
+exports.register = function (server, options, next) {
+
+    const db = server.app.db;
+    
+    server.route({
+        method: 'GET',
+        path: '/search/firstname/{patFirstName}/showpatients',
+        handler: function (request, reply) {
+            db.Patients.find({
+                patFirstName: request.params.patFirstName
+            }, (err, doc) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (!doc) {
+                    return reply(Boom.notFound());
+                }
+
+                reply(doc);
+            });
+
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/search/lastname/{patLastName}/showpatients',
+        handler: function (request, reply) {
+            db.Patients.find({
+                patLastName: request.params.patLastName
+            }, (err, doc) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (!doc) {
+                    return reply(Boom.notFound());
+                }
+
+                reply(doc);
+            });
+
+        }
+    });
+
+    return next();
+};
+
+exports.register.attributes = {
+    name: 'routes-search'
+};
