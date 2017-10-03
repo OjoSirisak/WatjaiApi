2af2829b41cfa179678b44cea105ce6eb26c6033
OjoSirisak <57130500078@st.sit.kmutt.ac.th>
2017-10-03 16:49:15 +0700

    update noti api

--- routes/watjaialert.js
+++ routes/watjaialert.js
@@ -158,6 +158,29 @@ exports.register = function (server, options, next) {
 
     server.route({
         method: 'GET',
+        path: '/watjaimeasure/showabnormal/{patId}/after/',
+        handler: function (request, reply) {
+            db.WatjaiMeasure.find({
+                patId: request.params.patId,
+                "abnormalStatus" : false
+            }, (err, docs) => {
+
+                if (err) {
+                    return reply(Boom.wrap(err, 'Internal MongoDB error'));
+                }
+
+                if (!docs) {
+                    return reply(Boom.notFound());
+                }
+
+                reply(docs);
+            });
+
+        }
+    });
+
+    server.route({
+        method: 'GET',
         path: '/watjaimeasure/showabnormal/{patId}/after/{measuringId}',
         handler: function (request, reply) {
             db.WatjaiMeasure.find({
