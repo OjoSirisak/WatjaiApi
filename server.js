5e3890662a2ba5072604a475c14c7d790874a421
OjoSirisak <57130500078@st.sit.kmutt.ac.th>
2018-03-06 22:52:59 +0700

    update server db

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ const mongojs = require('mongojs');
 const server = new Hapi.Server();
 server.connection({
     //host: 'localhost',
-    host: '192.168.137.1',
-    //host: '128.199.245.253', 
+    //host: '192.168.137.1',
+    //host: '139.59.98.254', 
     port: 3000
 });
 
 //Connect to db
-//server.app.db = mongojs('project:smartheart@128.199.245.253/Project?authMechanism=SCRAM-SHA-1', ['Doctors','Patients','WatjaiNormal','WatjaiAlert']);
-server.app.db = mongojs('192.168.80.130/Project', ['Doctors','Patients','WatjaiNormal','WatjaiAlert']);
+server.app.db = mongojs('project:smartheart@139.59.98.254/Project?authMechanism=SCRAM-SHA-1', ['Doctors','Patients','WatjaiNormal','WatjaiAlert']);
+//server.app.db = mongojs('192.168.80.130/Project', ['Doctors','Patients','WatjaiNormal','WatjaiAlert']);
 //Load plugins and start server
 server.route({
   method: 'GET',
